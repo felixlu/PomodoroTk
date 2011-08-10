204c13ade72c8df21f80b77d2d946866f12e7e1f
Felix Lu <lufelixgh@gmail.com>
2011-08-10 21:10:48 +0800

    rename CLI

--- pyPomodoroCLI.py
+++ pyPomodoroCLI.py
@@ -1,19 +1,21 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 
-
 import time
 
 
 def main():
-    work_time = get_int_input("Please enter an Integer for the Pomodoro (default 25 minutes): ", 25)
-    rest_time = get_int_input("Please enter an Integer for the Rest time (default 5 minutes): ", 5)
-    
-    count_down(work_time, "Pomodoro time left: {0}")
-    print("It's time to rest (for {0} minutes)! Please press Enter.".format(rest_time))
+    work_time = 60 * get_int_input(
+        "Please enter an Integer for the Pomodoro (default 25 minutes): ", 25)
+    rest_time = 60 * get_int_input(
+        "Please enter an Integer for the Rest time (default 5 minutes): ", 5)
+
+    count_down(work_time, "Pomodoro time left {0}")
+    print("Press Enter to start rest {0}".format(
+        time_format(rest_time)))
     input()
 
-    count_down(rest_time, "Rest time left: {0}")
+    count_down(rest_time, "Rest time left {0}")
     print("Go back to work! Please press Enter.")
     input()
 
@@ -22,9 +24,9 @@ def main():
 
 def get_int_input(msg, default_value):
     int_value = default_value
-    
+
     print(msg)
-    
+
     while True:
         line = input()
         if line:
@@ -41,15 +43,15 @@ def get_int_input(msg, default_value):
     return int_value
 
 
-def count_down(totalTime, msg):
-    for pastTime in range(totalTime):
-        print(msg.format(time_format(totalTime - pastTime)))
+def count_down(total_time, msg):
+    for past_time in range(total_time):
+        print(msg.format(time_format(total_time - past_time)))
         time.sleep(1)
 
 
-def time_format(timeInSeconds):
-    minutes = timeInSeconds // 60
-    seconds = timeInSeconds % 60
+def time_format(time_in_seconds):
+    minutes = time_in_seconds // 60
+    seconds = time_in_seconds % 60
     return "{0:0>2}:{1:0>2}".format(minutes, seconds)
 
 
