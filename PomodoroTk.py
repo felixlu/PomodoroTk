afbe83bbac1a9b9e173a661cf6248586ab104d7b
Felix Lu <lufelixgh@gmail.com>
2011-09-10 00:10:10 +0800

    Edit and Save task buttons are work now.

--- PomodoroTk.py
+++ PomodoroTk.py
@@ -20,8 +20,8 @@ import time
 
 class PomodoroTk(Frame):
 
-    # create widgets
     def create_widgets(self):
+    # create widgets
         # create Pomodoro label and input field
         self.pomodoro_lbl = Label(self.parent, text='Pomodoro:')
         self.pomodoro_lbl.grid(row=0, column=0, sticky='E')
@@ -43,7 +43,7 @@ class PomodoroTk(Frame):
             textvariable=self.rest_time_value)
         self.rest_time_entry.grid(row=1, column=1)
 
-        self.rest_time_min_lbl = Label(self.parent,text='minutes')
+        self.rest_time_min_lbl = Label(self.parent, text='minutes')
         self.rest_time_min_lbl.grid(row=1, column=2, sticky='W')
 
         # create Cycle label and input field
@@ -51,7 +51,7 @@ class PomodoroTk(Frame):
         self.cycle_lbl.grid(row=2, column=0, sticky='E')
 
         self.cycle_value = StringVar()
-        self.cycle_entry = Entry(self.parent, justify='right', 
+        self.cycle_entry = Entry(self.parent, justify='right',
             textvariable=self.cycle_value)
         self.cycle_entry.grid(row=2, column=1)
 
@@ -61,7 +61,7 @@ class PomodoroTk(Frame):
 
         # left time and status
         self.left_time_value = StringVar()
-        self.left_time_lbl = Label(self.parent, 
+        self.left_time_lbl = Label(self.parent,
             textvariable=self.left_time_value, font=('monospace', 12, 'bold'))
         self.left_time_lbl.grid(row=5, column=0, columnspan=4, sticky='EWNS')
 
@@ -78,7 +78,7 @@ class PomodoroTk(Frame):
 
         # pomodoro list
         self.pomodoro_list = MultiListbox(self.parent,
-            (('No.', 5), ('Task', 30), ('Status', 10), ('Date', 10), 
+            (('No.', 5), ('Task', 30), ('Status', 10), ('Date', 10),
                 ('Start', 10), ('Minutes', 7)))
         self.pomodoro_list.grid(row=4, column=0, columnspan=4)
 
@@ -89,10 +89,10 @@ class PomodoroTk(Frame):
         self.left_time_value.set('{0:<10}{1:>8}'.format(
             self.STATUS_IDLE, '00:00'))
 
-    # update widgets' status
     def update_widgets(self, status):
-        self.left_time_value.set('{0:<10}{1:>8}'.format(status, 
-            self.time_format(self.left_time)))
+    # update widgets' status
+        self.left_time_value.set('{0:<10}{1:>8}'.format(status,
+            time_format(self.left_time)))
         if status == self.STATUS_IDLE:
             self.start_btn.configure(text=self.LBL_START)
             self.left_time_lbl.configure(bg=self.COLOR_IDLE)
@@ -104,52 +104,48 @@ class PomodoroTk(Frame):
         else:
             pass
 
-    # command of Start/Cancel button
     def start_cmd(self):
-        # start from Idle
+    # command of Start/Cancel button
         if self.status == self.STATUS_IDLE:
+        # start from Idle
             if not self.continue_cycle:
                 # get verified input
-                self.pomodoro_time = self.get_int(
+                self.pomodoro_time = get_int(
                     self.pomodoro_time_value.get()) * 60
-                self.rest_time = self.get_int(self.rest_time_value.get()) * 60
-                self.cycle_count = self.get_int(self.cycle_value.get())
+                self.rest_time = get_int(self.rest_time_value.get()) * 60
+                self.cycle_count = get_int(self.cycle_value.get())
 
             if self.pomodoro_time > 0:
                 if self.rest_time > 0:
-                    # get task info
-                    self.task_content = self.get_task()
-                    if self.task_content:
-                        # get start time
-                        self.task_start_time = time.time()
-                        # reset status and indicator
-                        self.status = self.STATUS_WORKING
-                        self.left_time = self.pomodoro_time
-                        self.update_widgets(self.status)
-                        # start count down for Pomodoro
-                        self.update()
-                    else:
-                        self.invalid_input_msg_of('Task')
+                    # get start time
+                    self.task_start_time = time.time()
+                    # reset status and indicator
+                    self.status = self.STATUS_WORKING
+                    self.left_time = self.pomodoro_time
+                    self.update_widgets(self.status)
+                    # start count down for Pomodoro
+                    self.update()
                 else:
                     self.invalid_input_msg_of('Rest Time')
             else:
                 self.invalid_input_msg_of('Pomodoro')
 
-        # cancel
         elif self.status == self.STATUS_WORKING:
+        # cancel work
             self.cancel_cmd(self.STATUS_W_CANCELLED)
-            
+
         elif self.status == self.STATUS_RESTING:
+        # cancel rest
             self.cancel_cmd(self.STATUS_R_CANCELLED)
 
-        # to be extended
         else:
+        # to be extended
             pass
 
-    # command of Cancel button
     def cancel_cmd(self, status):
+    # command of Cancel button
         self.left_time_lbl.after_cancel(self.after_id)
-        if askokcancel('Question', 
+        if askokcancel('Question',
             'Do you really want to cancel the whole Pomodoro cycle now?'):
                 # get end time
                 self.end_time = time.strftime('%H:%M:%S')
@@ -160,29 +156,42 @@ class PomodoroTk(Frame):
                 self.left_time = 0
         self.update()
 
-    # command of Edit button
     def edit_cmd(self):
-        pass
+    # command of Edit button
+        tid = self.get_selected_task_id()
+        if tid != None:
+            self.task_value.set(get_task_by_id(self.con, self.cur, tid))
+            self.editing_task_id = tid
 
-    # command of Save button
     def save_cmd(self):
-        pass
+    # command of Save button
+        self.task_content = self.task_value.get()
+        if self.task_content:
+            if self.editing_task_id != -1:
+                task = (self.editing_task_id, ) + (self.task_content, )
+                update_task(self.con, self.cur, task)
+                self.refresh_task_list()
+                self.editing_task_id = -1
+        else:
+            showerror('Invalid input', '"Task" can not be empty!')
 
-    # count down
     def update(self):
+    # count down
         # update Left Time label
         self.left_time_value.set('{0:<10}{1:>8}'.format(
-             self.status, self.time_format(self.left_time)))
-        # Pomodoro or Rest not finished
+             self.status, time_format(self.left_time)))
+
         if self.left_time > 0:
+        # Pomodoro or Rest not finished
             self.left_time -= 1
             self.after_id = self.left_time_lbl.after(1000, self.update)
-        # Pomodoro finished
+
         elif self.status == self.STATUS_WORKING:
+        # Pomodoro finished
             if self.cycle_count > 1:
-                showinfo('Information', 
+                showinfo('Information',
                     'You have worked for {0}, have a break now.'.format(
-                        self.time_format(self.pomodoro_time)))
+                        time_format(self.pomodoro_time)))
                 # get task end time and insert to db
                 self.task_finished(True)
                 # reset Left Time to Rest Time
@@ -200,8 +209,9 @@ class PomodoroTk(Frame):
                 self.continue_cycle = False
                 self.status = self.STATUS_IDLE
                 self.update_widgets(self.status)
-        # Rest finished
+
         elif self.status == self.STATUS_RESTING:
+        # Rest finished
             self.cycle_count -= 1
             self.status = self.STATUS_IDLE
             if askokcancel('Question',
@@ -211,59 +221,39 @@ class PomodoroTk(Frame):
                         self.start_cmd()
             else:
                 pass
-        # Cancelled
+
         elif self.status == self.STATUS_W_CANCELLED:
+        # Cancel work
             self.task_finished(False)
             self.status = self.STATUS_IDLE
             self.update_widgets(self.status)
+
         elif self.status == self.STATUS_R_CANCELLED:
+        # Cancel rest
             self.status = self.STATUS_IDLE
             self.update_widgets(self.status)
+
         else:
             pass
 
-    # verify input value
-    def get_int(self, var_input):
-        var_int = 0
-        if var_input:
-            try:
-                var_int = int(var_input)
-            except Exception as e:
-                print(e)
-        else:
-            pass
-        return var_int
-
-    # display message for invalid input
     def invalid_input_msg_of(self, var_name):
-        showerror('Invalid Input', 
+    # display message for invalid input
+        showerror('Invalid Input',
             'Invalid input. "{0}" must be Integer and greater than 0.'.format(
                 var_name))
 
-    # convert the second count into HH:MM:SS format
-    def time_format(self, time_in_sec):
-        time_hrs = time_in_sec // 3600
-        time_sec = time_in_sec % 60
-        if time_hrs:
-            time_min = (time_in_sec - time_hrs * 3600) // 60
-            return '{0}:{1:0>2}:{2:0>2}'.format(time_hrs, time_min, time_sec)
-        else:
-            time_min = time_in_sec // 60
-            return '{0:0>2}:{1:0>2}'.format(time_min, time_sec)
-
-    
-    def get_task(self):
-        task = self.task_value.get()
-        if task:
-            return task
-        else:
-            return None
-    
+#    def get_task(self):
+#        task = self.task_value.get()
+#        if task:
+#            return task
+#        else:
+#            return None
     def task_finished(self, task_is_valid):
         if task_is_valid:
             self.task_is_valid = 1
         else:
             self.task_is_valid = 0
+        self.task_content = self.task_value.get()
         self.task_end_time = time.time()
         date = time.strftime('%Y-%m-%d', time.localtime(self.task_start_time))
         start = time.strftime('%H:%M:%S', time.localtime(self.task_start_time))
@@ -271,7 +261,7 @@ class PomodoroTk(Frame):
         task = (self.task_content, self.task_is_valid, date, start, minutes)
         insert_task(con, cur, task)
         self.refresh_task_list()
-    
+
     def refresh_task_list(self):
         list_size = self.pomodoro_list.size()
         if list_size > 0:
@@ -282,8 +272,15 @@ class PomodoroTk(Frame):
         for row in rows:
             self.number_to_id.append(row[0])
             i += 1
-            self.pomodoro_list.insert(self.pomodoro_list.size(), 
+            self.pomodoro_list.insert(self.pomodoro_list.size(),
                 (i, row[1], self.TASK_STATUS[row[2]], row[3], row[4], row[5]))
+
+    def get_selected_task_id(self):
+        if len(self.pomodoro_list.curselection()) > 0:
+            list_no = int(self.pomodoro_list.curselection()[0])
+            return self.number_to_id[list_no]
+        else:
+            return None
 
     def __init__(self, parent, con, cur):
         super().__init__(parent)
@@ -304,11 +301,10 @@ class PomodoroTk(Frame):
         self.STATUS_RESTING = 'Resting'
         self.STATUS_W_CANCELLED = 'Work Cancelled'
         self.STATUS_R_CANCELLED = 'Rest Cancelled'
-        
+
         self.TASK_STATUS = ['Cancelled', 'Finished']
 
         self.create_widgets()
-
 
         # data initialize
         self.pomodoro_time = 0
@@ -323,15 +319,41 @@ class PomodoroTk(Frame):
         self.task_is_valid = 0
         self.task_start_time = None
         self.task_end_time = None
-        
+        self.editing_task_id = -1
+
         self.number_to_id = []
 
         self.update_widgets(self.status)
         self.refresh_task_list()
 
 
-# database initialize
+def time_format(time_in_sec):
+    # convert the second count into HH:MM:SS format
+    time_hrs = time_in_sec // 3600
+    time_sec = time_in_sec % 60
+    if time_hrs:
+        time_min = (time_in_sec - time_hrs * 3600) // 60
+        return '{0}:{1:0>2}:{2:0>2}'.format(time_hrs, time_min, time_sec)
+    else:
+        time_min = time_in_sec // 60
+        return '{0:0>2}:{1:0>2}'.format(time_min, time_sec)
+
+
+def get_int(var_input):
+    # verify input value
+    var_int = 0
+    if var_input:
+        try:
+            var_int = int(var_input)
+        except Exception as e:
+            print(e)
+    else:
+        pass
+    return var_int
+
+
 def init_db(con, cur):
+    # database initialize
     cur.execute("""
         CREATE TABLE Pomodoro (
             id         integer primary key autoincrement,
@@ -340,31 +362,67 @@ def init_db(con, cur):
             date       text,
             start_time text,
             duration   integer
-        );
-    """)
+            );
+        """)
     con.commit()
 
+
 def get_all_tasks(cur):
-    query = """SELECT id, task, is_valid, date, start_time, duration 
-        FROM Pomodoro;"""
+    query = """SELECT id, task, is_valid, date, start_time, duration
+        FROM Pomodoro;
+        """
     cur.execute(query)
     return cur.fetchall()
-    
+
+
 def insert_task(con, cur, task):
     query = """
-        INSERT INTO Pomodoro (task, is_valid, date, start_time, duration)
-        VALUES('{0[0]}', '{0[1]}', '{0[2]}', '{0[3]}', '{0[4]}');""".format(task)
+        INSERT INTO Pomodoro
+        (task, is_valid, date, start_time, duration)
+        VALUES
+        ('{0[0]}', '{0[1]}', '{0[2]}', '{0[3]}', '{0[4]}');
+        """.format(task)
     cur.execute(query)
     con.commit()
+
+
+def get_task_by_id(con, cur, task_id):
+    query = """
+        SELECT id, task FROM Pomodoro
+        WHERE id='{0}';
+        """.format(task_id)
+    cur.execute(query)
+    row = cur.fetchone()
+    return row[1]
+
+
+def update_task(con, cur, task):
+    query = """
+        UPDATE Pomodoro
+        SET task='{0[1]}'
+        WHERE id='{0[0]}';
+        """.format(task)
+    cur.execute(query)
+    con.commit()
+
+
+def delete_task(con, cur, task_id):
+    query = """
+        DELETE FROM Task
+        WHERE id='{0}';
+        """.format(task_id)
+    cur.execute(query)
+    con.commit()
+
 
 if __name__ == '__main__':
     db_name = 'pomodoroTk.db'
     db_path = os.path.join(os.path.expanduser('~'), db_name)
-    
+
     needs_init = False
     if not os.path.isfile(db_path):
         needs_init = True
-    
+
     con = sqlite3.connect(db_path)
     con.isolation_level = None
     cur = con.cursor()
@@ -374,7 +432,7 @@ if __name__ == '__main__':
             init_db(con, cur)
         except Exception as e:
             print(e)
-    
+
     app = Tk()
     app.title('PomodoroTk')
     window = PomodoroTk(app, con, cur)
